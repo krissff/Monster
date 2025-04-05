9dd167ffec85cfd3d6144fe977f04f33d7638644
Kelly Cristina <kellycristinaff@gmail.com+>
2025-04-05 20:02:54 -0300

    Initial commit

---
+++ Main.py
@@ -0,0 +1,18 @@
+import pygame
+
+print('Setup Start')
+pygame.init()
+window = pygame.display.set_mode(size=(600, 480))
+print('Setup End')
+
+print('Loop Start')
+while True:
+    # Checar todos os eventos
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()  # Fechar janela
+            quit()  #Encerrar pygame
+
+
+
+
